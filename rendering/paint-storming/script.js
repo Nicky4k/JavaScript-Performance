4fdf70cfe5681cb7c82b01bdb2d4b7e4f02f4114
Nicky <nickysauceofficial@gmail.com>
2023-04-22 22:41:09 +0530

    painting, will-change, compositing thread, main thread, layout thrashing, paint storm

--- rendering/paint-storming/script.js
+++ rendering/paint-storming/script.js
@@ -1,7 +1,30 @@
-const $box = $('.box');
+// const $box = $(".box");
 
-$box.on('click', () => {
-  $box.animate({
-    marginLeft: '500px',
-  }, 500);
+// $box.on("click", () => {
+//   $box.animate(
+//     {
+//       marginLeft: "500px",
+//     },
+//     500
+//   );
+// });
+
+const boxEl = document.querySelector(".box");
+
+boxEl.addEventListener("mouseenter", () => {
+  boxEl.style.willChange = "transform";
+});
+
+boxEl.addEventListener("click", () => {
+  boxEl.classList.toggle("move");
+});
+
+boxEl.addEventListener("mouseleave", () => {
+  boxEl.style.willChange = "auto";
+  console.log("mouse leave");
+});
+
+boxEl.addEventListener("transitionend", () => {
+  boxEl.style.willChange = "auto";
+  console.log("transition end");
 });
